3b5739d038b2bce115e6c2e03876bb0c3fe0eed5
ely <ely@elyandpilar.com>
2022-02-01 21:20:18 -0800

    playlist screen shows dialog while waiting for link.

--- admin/pages/playlists.tsx
+++ admin/pages/playlists.tsx
@@ -1,10 +1,9 @@
 import Layout from '../components/layout'
 import { getSession, useSession, signIn, signOut } from "next-auth/react"
 import {getUsersPlaylists} from '../lib/spotify';
-import { Table, Space, Button } from 'antd';
+import { Table, Space, Button, Modal } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
 import { useState } from 'react';
-import { json } from 'stream/consumers';
 
 
 interface PlaylistData {
@@ -32,27 +31,43 @@ export async function getServerSideProps(ctx) {
 export default function Playlists({items}) {
   const { data: session } = useSession()
   const [linkData,setLinkData] = useState(undefined);
+  const [timeoutID,setTimeoutID] = useState(undefined);
 
 
   const linkCard = async (record) => {
-    console.log(`linking record: ${record}`)
+    let body = (record)? JSON.stringify({
+      url:record.external_urls.spotify,
+      title:record.name,
+      cover:record.images[0]?.url,
+      details: {
+        printed: false
+      }
+    }):"{}";
+
     let response = await fetch('/api/card/link',{
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       method: "POST",
-      body: JSON.stringify({
-        url:record.external_urls.spotify,
-        title:record.name,
-        cover:record.images[0]?.url,
-        details: {
-          printed: false
-        }
-      })
+      body
     })  
-    let result = await response.json()
-    setLinkData(result);
+    if(record) {
+      let result = await response.json()
+      setLinkData(result);
+      setTimeoutID(setTimeout(handleCancelLink,30000));
+    } else {
+      setLinkData(undefined);
+      if(timeoutID != undefined) {
+        clearTimeout(timeoutID);
+        setTimeoutID(undefined);
+      }
+    }
+  }
+
+  const handleCancelLink = async () => {
+    setLinkData(undefined);
+    linkCard(undefined);
   }
 
   const columns: ColumnsType<PlaylistData> = [
@@ -83,7 +98,22 @@ export default function Playlists({items}) {
     return (
     <section>
       <h1>Playlists</h1>
-        <Table columns={columns} pagination={{/*pageSize: 15*/}} dataSource={items} />      
+        <Table columns={columns} pagination={{/*pageSize: 15*/}} dataSource={items} />          
+        <Modal
+          visible={linkData != undefined}
+          title="Ready to Link"
+          onCancel={handleCancelLink}
+          footer={[
+            <Button key="back" onClick={handleCancelLink}>
+              Cancel
+            </Button>,
+          ]}
+        >
+         <div>
+           <p>Tap a card on tappy to link it to this playlist.</p>
+         </div>
+        </Modal>
+
     </section>
   )
   }
