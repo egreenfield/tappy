a0a14a8fc955df87f1f5fb751e6a51c7fcc26c02
ely <ely@elyandpilar.com>
2022-02-03 16:31:50 -0800

    first pass at pdf test

--- admin/pages/api/print.ts
+++ admin/pages/api/print.ts
@@ -1,7 +1,87 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import PdfPrinter from 'pdfmake';
 import { getCurrentCards } from '../../lib/serverCardActions'
 
+var fonts = {
+	Roboto: {
+		normal: 'fonts/Roboto-Regular.ttf',
+		bold: 'fonts/Roboto-Medium.ttf',
+		italics: 'fonts/Roboto-Italic.ttf',
+		bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+	}
+};
 
+
+function mmToPt(mm:number) {
+    return mm*2.83465
+}
+
+async function  getDoc() {
+
+    const response = await fetch('https://i.scdn.co/image/ab67706f00000003a2dba0d1f56bc9bdb28b5204');
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const b64 = "data:image/jpeg;base64," + buffer.toString("base64url")
+    console.log("url is",b64);
+
+    var docDefinition = {
+        pageSize: 'LETTER',
+        pageMargins: [ 72/4, 72/4, 72/4, 72/4 ],
+        content: [
+            {
+                svg: 
+                `<svg>
+                    <rect width="54mm" stroke="black" stroke-width="1" fill-opacity="0" height="85mm" rx="2mm" ry="2mm"/>
+                </svg>`,
+                absolutePosition: { x: 10, y: 10 }
+            },
+            { 
+                image: 'img1',
+                width: mmToPt(54),
+                height: mmToPt(54),
+                absolutePosition: { x: 10, y: 10 }
+            },
+            {
+                text: 'This is a long title that is part of a text block This is a long title that is part of a text block',
+                absolutePosition: { x: 10, y: mmToPt(54) },
+                width: mmToPt(54),
+                height: mmToPt(85-54),
+            },
+        ],
+        images: {
+            img1: b64,
+        },
+        styles: {
+            header: {
+                fontSize: 18,
+                bold: true,
+                margin: [0, 0, 0, 10]
+            },
+            subheader: {
+                fontSize: 16,
+                bold: true,
+                margin: [0, 10, 0, 5]
+            },
+            tableExample: {
+                margin: [0, 5, 0, 15]
+            },
+            tableHeader: {
+                bold: true,
+                fontSize: 13,
+                color: 'black'
+            }
+        },
+    };
+    return docDefinition;
+}
+
+async function generatePDF(definition:any) {
+    
+    
+    var printer = new PdfPrinter(fonts);
+    var doc = printer.createPdfKitDocument(await getDoc() as any);
+    return doc;
+}
 
   
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
@@ -19,5 +99,15 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
     }
     let cardsToPrint = currentCards.filter(card => card.id in idsToPrintMap)
 
-    res.status(200).json(cardsToPrint)
+    let pdf;
+    try {
+        let pdf = await generatePDF(cardsToPrint);
+        res.status(200)
+        res.setHeader("Content-Type",'application/pdf');
+        pdf.pipe(res);
+        pdf.end();
+    } catch(e) {
+        console.log("error generating pdf:",e);
+        res.status(500).json({error:"pdf generation failed"})
+    }
 }
